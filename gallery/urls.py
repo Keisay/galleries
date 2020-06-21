0b62448f6978409a0672aaae47097d62e1002d69
Keisay <kinsay.soundcloud@gmail.com>
2020-06-21 18:14:11 +0200

    Add a new gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name='home'),
+    path('waraku', views.index, name='index'),
+    path('videos', views.index2, name='index2'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -4,8 +4,19 @@ from django.conf import settings
 from django.templatetags.static import static
 
 # Create your views here.
+
+def home(request):
+    return render(request, "gallery/home.html")
+
 def index(request):
     path = settings.MEDIA_ROOT
     img_list = os.listdir(path + "/imgs")
     context = {"imgs": img_list}
     return render(request, "gallery/index.html", context)
+
+def index2(request):
+    path = settings.MEDIA_ROOT
+    vid_list = os.listdir(path + "/vids")
+    context = {"vids": vid_list}
+    return render(request, "gallery/index2.html", context)
+
