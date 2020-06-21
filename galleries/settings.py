0066c745f2385b852ff5f4de31c6d3dd0274d887
Keisay <kinsay.soundcloud@gmail.com>
2020-06-21 17:38:55 +0200

    Add a new gallery

--- galleries/settings.py
+++ galleries/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.media',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = 'media/'
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
 
 # Create your views here.
+def index(request):
+    path = settings.MEDIA_ROOT
+    img_list = os.listdir(path + "/imgs")
+    context = {"imgs": img_list}
+    return render(request, "gallery/index.html", context)
